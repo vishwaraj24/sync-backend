935390da8eae35b8b1c429716071d12628918a56
vishwaraj24 <vishwarajrajebhonsle@gmail.com>
2026-04-05 20:10:20 +0530

    fix mongoose connection

--- config/db.js
+++ config/db.js
@@ -2,14 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-
-        console.log('✅ MongoDB Connected to syncDB');
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB Connected to syncDB');
     } catch (error) {
-        console.error('❌ MongoDB connection error:', error.message);
+        console.error('MongoDB connection error:', error);
         process.exit(1);
     }
 };
