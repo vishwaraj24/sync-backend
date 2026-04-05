d15d764d00d86089685c01f4acf9f55e89159a73
vishwaraj24 <vishwarajrajebhonsle@gmail.com>
2026-04-05 20:18:14 +0530

    fix mongoose options error

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB Connected to syncDB');
     } catch (error) {
-        console.error('MongoDB connection error:', error);
+        console.error('❌ MongoDB connection error:', error.message);
         process.exit(1);
     }
 };
